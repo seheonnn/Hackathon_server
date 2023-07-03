a2513a14e39381110f297a5d2d0b4bd71b9e3cd7
YuJin Kwon <tokyj515@naver.com>
2023-07-03 20:06:20 +0900

    Feat: 클래스 생성

---
+++ src/main/java/com/umc/demo2/controller/TreasureController.java
@@ -0,0 +1,11 @@
+package com.umc.demo2.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/treasure")
+public class TreasureController {
+}
---
+++ src/main/java/com/umc/demo2/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.umc.demo2.repository;
+
+import com.umc.demo2.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
