f94ee5edc6f3885fa53e0687b1dbcc20b474705d
KellyKimHyeJin <kelly317kim@naver.com>
2023-07-03 20:16:42 +0900

    feat: UserTreasure

--- src/main/java/com/umc/demo2/controller/UserTreasureController.java
+++ src/main/java/com/umc/demo2/controller/UserTreasureController.java
@@ -1,11 +1,17 @@
 package com.umc.demo2.controller;
 
+import com.umc.demo2.dto.UserTreasureResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/")
 public class UserTreasureController {
+
+    @PatchMapping("/comment/{userId}/{treasureId}")
+    public ResponseEntity<UserTreasureResponseDto.UpdateUserTreasureDto> updateUserTreasure(@PathVariable(name = "userId")Long userId, @PathVariable(name ="treasureId")Long treasureId, @RequestBody UserTreasureRequestDto.UpdateUserTreasureDto request){
+
+    }
 }
---
+++ src/main/java/com/umc/demo2/dto/UserTreasureRequestDto.java
@@ -0,0 +1,11 @@
+package com.umc.demo2.dto;
+
+import lombok.Getter;
+
+public class UserTreasureRequestDto {
+
+    @Getter
+    public static class UpdateUserTreasureDto{
+
+    }
+}
