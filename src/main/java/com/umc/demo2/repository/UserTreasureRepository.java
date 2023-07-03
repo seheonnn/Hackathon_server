0d4c4234d93247f3fe23a4b972c687836d486e8a
YuJin Kwon <tokyj515@naver.com>
2023-07-03 21:08:40 +0900

    Feat: 유저의 보물 리스트 조회

--- src/main/java/com/umc/demo2/repository/UserTreasureRepository.java
+++ src/main/java/com/umc/demo2/repository/UserTreasureRepository.java
@@ -4,6 +4,12 @@ import com.umc.demo2.domain.UserTreasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserTreasureRepository extends JpaRepository<UserTreasure, Long> {
+
+    List<UserTreasure> findAllByUserId(Long userId);
+
+    Long countAllByUserId(Long userId);
 }
--- src/main/java/com/umc/demo2/service/UserTreasureService.java
+++ src/main/java/com/umc/demo2/service/UserTreasureService.java
@@ -1,9 +1,51 @@
 package com.umc.demo2.service;
 
+import com.umc.demo2.domain.Treasure;
+import com.umc.demo2.domain.User;
+import com.umc.demo2.domain.UserTreasure;
+import com.umc.demo2.dto.TreasureRes;
+import com.umc.demo2.repository.TreasureRepository;
+import com.umc.demo2.repository.UserRepository;
+import com.umc.demo2.repository.UserTreasureRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserTreasureService {
+
+    private final UserTreasureRepository userTreasureRepository;
+
+    private final UserRepository userRepository;
+
+    private final TreasureRepository treasureRepository;
+
+
+    public List<TreasureRes.UserTreasureRes> getUserTreasureList(Long userId){
+        List<TreasureRes.UserTreasureRes> result = new ArrayList<>();
+        List<UserTreasure> userTreasures = userTreasureRepository.findAllByUserId(userId);
+
+        User user = userRepository.findUserByUserId(userId);
+
+        for(UserTreasure u : userTreasures){
+            Treasure treasure = treasureRepository.findByTreasureId(u.getTreasureId());
+
+            TreasureRes.UserTreasureRes element =
+                    new TreasureRes.UserTreasureRes( treasure.getTitle(), treasure.getContent());
+
+            result.add(element);
+        }
+
+        return result;
+    }
+
+    public Long countUserTreasureList(Long userId){
+        //List<UserTreasure> userTreasures = userTreasureRepository.findAllByUserId(userId);
+        Long cnt = userTreasureRepository.countAllByUserId(userId);
+
+        return cnt;
+    }
 }
