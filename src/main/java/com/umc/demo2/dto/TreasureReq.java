bbd26d9631cae4ca17749aaa272921257d9c5826
YuJin Kwon <tokyj515@naver.com>
2023-07-03 20:32:22 +0900

    Feat: dto 클래스 생성

---
+++ src/main/java/com/umc/demo2/dto/TreasureReq.java
@@ -0,0 +1,11 @@
+package com.umc.demo2.dto;
+
+
+public class TreasureReq {
+    //    @Builder
+    //    @Getter
+    //    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    //    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+
+
+}
