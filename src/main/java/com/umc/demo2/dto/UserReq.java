044811572dd990c947fe5d09de67aea7f61948ad
KellyKimHyeJin <kelly317kim@naver.com>
2023-07-04 00:02:44 +0900

    feat: 닉네임 수정

--- src/main/java/com/umc/demo2/dto/UserReq.java
+++ src/main/java/com/umc/demo2/dto/UserReq.java
@@ -1,4 +1,15 @@
 package com.umc.demo2.dto;
 
+import lombok.*;
+
 public class UserReq {
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    public static class UpdateNickname{
+        private String nickname;
+    }
+
 }
