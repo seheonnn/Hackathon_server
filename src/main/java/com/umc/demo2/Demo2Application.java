dc5506500fca4cefd0992b3836007c5b030877e9
YuJin Kwon <tokyj515@naver.com>
2023-07-03 13:55:20 +0900

    Feat: 기본 환경 설정

--- src/main/java/com/umc/demo2/Demo2Application.java
+++ src/main/java/com/umc/demo2/Demo2Application.java
@@ -2,6 +2,7 @@ package com.umc.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class Demo2Application {
